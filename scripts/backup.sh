cbcd83803adb9fffc2c282ecde6d0261f93f4f38
aiganysh <aiganysh.abdukaarova@alatoo.edu.kg>
2026-01-30 15:01:17 +0600

    Complete Lab 1: Linux CLI

---
+++ scripts/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Creating backup..."
+mkdir -p backup
+cp -r src backup/
+
---
+++ scripts/hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "================================"
+echo "DevOps lab 1 - System Information"
+echo "================================="
+echo "Current User: $USER"
+echo "Current Directory: $ (pwd)"
+echo "Date: $ (date)"
+echo "================================"
